24c7c752e83116cc3b6433cd99c601728050c977
Nicola-Campagna <campagnanicola379@gmail.com>
2023-05-17 11:47:40 +0200

    separato funzione genera griglia e funzione genera cella + refactoring

--- main.js
+++ main.js
@@ -8,9 +8,8 @@ btnEl.addEventListener(
         // tabella di gioco
         const gridEl = document.getElementById("tabella");
         // numero celle delle tabella
-        const dimensionGridEl = 100;
         // invochiamo la funzione per generare la tabella di gioco
-        generateGrid(gridEl, dimensionGridEl);
+        generateGrid(gridEl, 1);
     }
 )
 
@@ -22,36 +21,47 @@ btnEl.addEventListener(
 /**
  * funzione che genera una griglia di gioco
  * @param {HTMLElement} grid tabella di gioco 
- * @param {int} dimension valore che indica numero di caselle
+ * @param {int} difficolta valore che indica in base al livello di difficolta il numero di caselle
  */
-function generateGrid(grid, dimension) {
+
+function generateGrid(grid, difficolta) {
     grid.innerHTML = "";
-    dimension = parseInt(dimension);
-
-    // assegnare alla cella i numeri da min a max (dimension)
-    const whiteList = [];
-    for (let i = 0; i < dimension; i++) {
-        whiteList.push(i + 1);
-    }
-
-    // ciclo per generare n cella in base al parametro dimension
-    for (let i = 0; i < dimension; i++) {
-        // creo l'elemento cella
-        const cella = document.createElement("div");
-        // aggiungo classe css alla cella
-        cella.classList.add("square");
-        // numero da 1 a 100 assegnati alle celle in ordine crescente
-        cella.append(whiteList[i]);
-        // al click la cella si colora all'interno e stampa un mex in console 
-        cella.addEventListener(
-            "click",
-            function () {
-                this.classList.toggle("active");
-                console.log("cella cliccata: " + (i + 1));
-            }
-        )
-        // appendo l'elemento cella nella tabella di gioco
+    // per 100 volte richiama la funzione generateCella
+    for (let i = 0; i < 100; i++) {
+        // assegniamo alla cella un numero progressivo da 1 a 100
+        const testoCella = (i + 1);
+        // variabile d'appoggio e invochiamo la funzione passando la i nella funzione generateCella attraverso il parametro
+        const cella = generateCella(difficolta, testoCella);
+        // appendo le celle create nella tabella nel DOM
         grid.append(cella);
     }
 
+}
+
+
+
+/**
+ * funzione che genera le celle da appendere successivamente in una tabella di gioco
+ * @param {int} difficolta paramentro che ci consente di verificare quante celle andranno create nel ciclo in base alla difficoltà
+ * @param {string} testo indica la i (numero progressivo da 1 a 100)
+ * @returns {HTMLElement} elemento creato nel ciclo (cella di gioco)
+*/
+function generateCella(difficolta, testo) {
+    // creo l'elemento cella
+    const cellaEl = document.createElement("div");
+    // aggiungo classe css alla cella
+    cellaEl.classList.add("square");
+    // la cella nel DOM avrà un numero progressivo da 1 a 100
+    cellaEl.innerHTML = testo;
+    // al click la cellaEl si accende o spegne e manda un mex in console
+    cellaEl.addEventListener(
+        "click",
+        function () {
+            this.classList.toggle("active");
+            console.log("cella cliccata: " + testo);
+        }
+    )
+
+    // ritorna la cella creata
+    return cellaEl;
 }
